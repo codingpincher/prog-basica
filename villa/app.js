e93ce6da120f8d3c5203d66e48419da4ceced0ec
codingpincher <codingpincher@gmail.com>
2018-11-06 04:18:12 +0100

    clase 21 a la mitad

--- villa/app.js
+++ villa/app.js
@@ -1,11 +1,31 @@
-for(var i = 0; i < 10; i++)
+var jsCanvas = document.getElementById("canvas");
+var contextCanvas = jsCanvas.getContext("2d");
+var img_mapa = "images/tile.png";
+var img_vaca = 'images/vaca.png'
+var x_aleatorio = aleatorio(0, 420);
+var y_aleatorio = aleatorio(0, 420);
+
+var fondo = new Image();
+fondo.src = img_mapa;
+fondo.addEventListener("load", dibujarFondo);
+
+var vaca = new Image();
+vaca.src = img_vaca;
+vaca.addEventListener("load", dibujarElemento)
+
+
+function dibujarElemento()
 {
-  var z;
-  z = aleatorio(10, 20);
-  document.write(z + ', ');
+  contextCanvas.drawImage(vaca, x_aleatorio, y_aleatorio);
 }
 
-function aleatorio (min, max)
+function dibujarFondo()
+{
+  contextCanvas.drawImage(fondo, 0, 0);
+}
+
+
+function aleatorio(min, max)
 {
   var n;
   n = Math.floor(Math.random() * (max - min + 1)) + min;

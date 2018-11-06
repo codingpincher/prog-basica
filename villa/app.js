43431fa30f80c9acc065ca658341a43958c4022f
codingpincher <codingpincher@gmail.com>
2018-11-06 03:06:01 +0100

    clase 20

--- villa/app.js
+++ villa/app.js
@@ -1,9 +1,13 @@
+for(var i = 0; i < 10; i++)
+{
+  var z;
+  z = aleatorio(10, 20);
+  document.write(z + ', ');
+}
+
 function aleatorio (min, max)
 {
   var n;
   n = Math.floor(Math.random() * (max - min + 1)) + min;
   return n;
 }
-
-var z = aleatorio(10, 20)
-document.write(z)
